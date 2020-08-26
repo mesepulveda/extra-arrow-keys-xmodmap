77e0b79f3c2fba56229f22aec22fa6298da670ff
Matías Sepúlveda <48467071+mesepulveda@users.noreply.github.com>
2020-08-25 23:51:15 -0400

    Shell script added

---
+++ map_keys.sh
@@ -0,0 +1,4 @@
+xmodmap -e "keycode  26 = e E e E Up Up EuroSign"
+xmodmap -e "keycode  39 = s S s S Left Left ssharp"
+xmodmap -e "keycode  40 = d D d D Down Down eth"
+xmodmap -e "keycode  41 = f F f F Right Right dstroke"
